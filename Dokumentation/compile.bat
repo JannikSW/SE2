084138ba7c3262705ea6c09576a18e81b6ca1850
JannikSW <jannik.scheller@wtnet.de>
2022-09-20 17:15:41 +0200

    initial

--- Dokumentation/compile.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem Compiles the document completely, including references, bibliography, and glossaries/acronyms.
-pdflatex 000_Praxisbericht.tex
-bibtex 000_Praxisbericht
-makeglossaries 000_Praxisbericht
-
-rem Call pdflatex twice after completing bibtex and makeglossaries, see also BibTeX and glossaries package documentation.
-pdflatex 000_Praxisbericht.tex
-pdflatex 000_Praxisbericht.tex
